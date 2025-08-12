5dff7cd33a2e540198d2153f505c8588cd9c3807
Anastasiya Yafimuk <a.yafimuk@gmail.com>
2025-08-12 13:47:36 +0300

    minor fix

--- python/lesson 5/read_file.py
+++ python/lesson 5/read_file.py
@@ -1,53 +1,56 @@
 # ====== ЧТЕНИЕ ======
 # "r" — чтение
-file = open(r"python\lesson 5\file.txt", "r")
+
+file_path = r"python/lesson 5/file.txt"
+
+file = open(file_path, "r")
 content = file.read()
 print(content)
 file.close()
 
 # Чтение одной строки (по очереди)
-file = open(r"python\lesson 5\file.txt", "r")
+file =open(file_path, "r")
 line = file.readline()
 print(line)
 file.close()
 
 # Чтение всех строк в список
-file = open(r"python\lesson 5\file.txt", "r")
+file =open(file_path, "r")
 lines = file.readlines()
 print(lines)
 file.close()
 
 # ====== ЗАПИСЬ ======
 # "w" — запись (перезаписывает или создаёт файл)
-file = open(r"python\lesson 5\file.txt", "w")
+file =open(file_path, "w")
 file.write("Катя 19")
 file.close()
 
-file = open(r"python\lesson 5\file.txt", "r")
+file =open(file_path, "r")
 content = file.read()
 print(content)
 file.close()
 
 
-file = open(r"python\lesson 5\file.txt", "w")
+file =open(file_path, "w")
 file.writelines(['Катя 19\n', 'Юля 26\n'])
 file.close()
 
-file = open(r"python\lesson 5\file.txt", "r")
+file =open(file_path, "r")
 content = file.read()
 print(content)
 file.close()
 
 # ====== ЧТЕНИЕ + ЗАПИСЬ ======
 # "r+" — файл должен существовать
-file = open(r"python\lesson 5\file.txt", "r+")
+file =open(file_path, "r+")
 file.write("Катя 19")
 content = file.read()
 print(content)
 file.close()
 
 # "a+" — добавление и чтение
-file = open(r"python\lesson 5\file.txt", "a+")
+file =open(file_path, "a+")
 file.writelines(["Катя 19\n"])
 file.seek(0) # ставит курсор в начало файла
 content = file.readlines()
@@ -57,7 +60,7 @@ print(content)
 
 
 try:
-    with open(r"python\lesson 5\file.txt", "r") as file:
+    with open(file_path, "r") as file:
         content = file.read()
         print(content)
 except FileNotFoundError:
@@ -75,7 +78,7 @@ finally:
 
 
 
-with open(r"python\lesson 5\file.txt", "r") as file:
+with open(file_path, "r") as file:
     persons = file.readlines()
     for person in persons:
         print(person)
